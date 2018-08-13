1ee2ee38be3a7809d3b42d263a235ef996c29607
Jose Chio <jchio@hugeinc.com>
2018-08-12 22:21:04 -0400

    add console: browser error article gitignore app manifest improve done session status on localforage

--- routes/articles/_articles.js
+++ routes/articles/_articles.js
@@ -1,7 +1,14 @@
+// TODO: see if these can be used to dynamically import and print
+// the components/articles on the index.html
 export const articles = [
   {
     id: "consoleopenpanel",
     title: "Console: Open Panel",
     component: "ConsoleOpenPanel"
+  },
+  {
+    id: "consolebrowsererror",
+    title: "Console: Browser Error",
+    component: "ConsoleBrowserError"
   }
 ];
