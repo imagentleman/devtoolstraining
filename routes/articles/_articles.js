9e0dde298f6472969e12bb5b66a856f5dbcb9375
Jose Chio <jchio@hugeinc.com>
2018-08-21 11:29:53 -0400

    add a bunch of console stuff (open as drawer, $0-$4, querying, stack traces, stopped execution, storing and copying and form validation).

--- routes/articles/_articles.js
+++ routes/articles/_articles.js
@@ -27,5 +27,42 @@ export const articles = [
     id: "consoledeverror",
     title: "Console: Dev Error",
     component: "ConsoleDevError"
+  },
+  {
+    id: "consolestoremanipulatecopy",
+    title: "Console: Store, Manipulate and Copy",
+    component: "ConsoleStoreManipulateCopy"
+  },
+  {
+    id: "consolestacktraces",
+    title: "Console: Stack Traces",
+    component: "ConsoleStackTraces"
+  },
+  {
+    id: "consoledrawer",
+    title: "Console: Drawer",
+    component: "ConsoleDrawer",
+    exercise: false
+  },
+  {
+    id: "consolestoppedexecution",
+    title: "Console: Stopped Execution",
+    component: "ConsoleStoppedExecution"
+  },
+  {
+    id: "consolequerying",
+    title: "Console: Querying the DOM ($ and $$)",
+    component: "ConsoleQuerying"
+  },
+  {
+    id: "consolelastfive",
+    title: "Console: $0 - $4 (last five)",
+    component: "ConsoleLastFive",
+    exercise: false
+  },
+  {
+    id: "consolevalidity",
+    title: "Console: checkvalidity()",
+    component: "ConsoleValidity"
   }
 ];
